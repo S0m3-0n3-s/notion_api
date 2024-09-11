43dd3cdfa856189296dbcd1fecd91c861d7fb9bd
AlpsXSR <quentin.michel@efrei.net>
2024-09-11 16:29:01 +0200

    fix js

--- TP1/script.js
+++ TP1/script.js
@@ -1,7 +1,15 @@
 const btn = document.getElementById("btn")
+const st = document.getElementById("st")
+const main_section = document.querySelector(".main_section")
 
 function handleClick(e) {
-	e.target.innerHTML = "bonjour"
+
+    st.innerText = "bouton cliqué !!"
+
+	st.classList.add("red-text")
+
 }
 
 btn.addEventListener("click", handleClick)
+
+//console.log("test ...", btn)
