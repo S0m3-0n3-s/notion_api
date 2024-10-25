6a498bd16e0e6a95214ec28b7c899a070bcbdecb
AlpsXSR <quentin.michel@efrei.net>
2024-10-25 15:19:50 +0200

    add api

--- TP4/index.js
+++ TP4/index.js
@@ -1,2 +1,81 @@
-console.log("test from node");
-console.log("test from node", Math.random(), new Date())
+//console.log("test from node");
+//console.log("test from node", Math.random(), new Date())
+
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+const users = [
+	{ id: 1, firstName: "John", lastName: "Doe", role: "admin" },
+	{ id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
+	{ id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
+	{ id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
+	{ id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
+]
+
+app.get("/", (req, res) => {
+	res.json(users)
+})
+
+app.post("/", (req, res) => {
+	const { firstName, lastName } = req.body
+	const lastId = users[users.length - 1].id
+	const newId = lastId + 1
+	const newUser = {
+		firstName,
+		lastName,
+		id: newId,
+	}
+	users.push(newUser)
+	res.status(201).json(newUser)
+})
+
+app.put("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex < 0) {
+        return res.status(404).json({ msg: "utilisateur non trouvé" });
+    }
+
+    const { firstName, lastName } = req.body;
+
+    if (firstName) users[userIndex].firstName = firstName;
+    if (lastName) users[userIndex].lastName = lastName;
+
+    res.json({
+        msg: "utilisateur mis à jour",
+        user: users[userIndex],
+    });
+});
+
+app.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex < 0) {
+        return res.status(404).json({ msg: "utilisateur non trouvé" });
+    }
+
+    users.splice(userIndex, 1);
+
+    res.json({
+        msg: "utilisateur supprimé",
+    });
+});
+
+app.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex < 0) {
+        return res.status(404).json({ msg: "utilisateur non trouvé" });
+    }
+
+    res.json(users[userIndex]);
+});
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
